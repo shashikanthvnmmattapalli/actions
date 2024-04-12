9b93bd6543919910b1ee56ca8a2b16caaefb856d
shashikanthvnmmattapalli <mattapallishashikanth@gmail.com>
2024-04-12 07:43:34 +0530

    update the code

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const port = 8080;
 const host = '0.0.0.0';
 
 app.get('/', (req, res) => {
-  res.send('Hello World from IBM Cloud');
+  res.send('Hello Shashikanth');
 })
 
 app.listen(port, host);
